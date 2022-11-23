933ccdf45cde1b8772f45e3bb34f63ebf2ee9ff1
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2022-11-23 13:42:20 -0300

    commit

--- src/modules/bibtex_reader.py
+++ src/modules/bibtex_reader.py
@@ -5,7 +5,7 @@ class BibtexReader:
 
     @staticmethod
     def read_files_to_dataframe(filepaths: list):
-        files = (open(path, encoding="utf-8") for path in filepaths)
+        files = (open(path, encoding="utf-8") for path in filepaths)  # type: ignore
         bibtexts = (load_bibtex(file) for file in files)
         dataframe_list = (pd.DataFrame(bib.entries) for bib in bibtexts)
         return pd.concat(dataframe_list)
